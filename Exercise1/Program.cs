5f0a9309521274012eed5777e0ec73b6e6139532
Janessa Aulén <S00190968@mail.itsligo.ie>
2019-02-20 16:28:47 +0000

    Exercise 1 finished.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryOp d1 = new BinaryOp(SimpleMaths.Add);
+            BinaryOp d2 = new BinaryOp(SimpleMaths.Subtract);
+
+            d1 += d2;
+            DoWork(d1);
+
+            Console.ReadLine();
+            
+        }
+        static void DoWork(BinaryOp d)
+        {
+            d(20, 10);
+        }
+    }
+}
---
+++ Exercise1/SimpleMaths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+
+    public delegate void BinaryOp(int x, int y);
+    public class SimpleMaths
+    {
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine($"The result of { x }+{ y } is: {x + y}.");
+        }
+
+        public static void Subtract(int x, int y)
+        {
+            Console.WriteLine($"The result of { x }-{ y } is: {x - y}.");
+        }
+    }
+}
