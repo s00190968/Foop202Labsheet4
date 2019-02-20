3c60d1a2900d9c89ae60384afbb93efd31274ff5
Janessa Aulén <S00190968@mail.itsligo.ie>
2019-02-20 17:36:26 +0000

    please github don't be an ass and work.

---
+++ Exercise6/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    public class Book : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        decimal price;
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                RaisePropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void RaisePropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
